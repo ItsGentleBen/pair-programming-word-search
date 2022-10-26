15d8e140efa0623c6bbdba10b8344bec95573598
Ben Stolb <benstolb@protonmail.com>
2022-10-26 16:47:06 -0700

    Completed wordsearch by Ilia and Ben

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//transpose function from Ben and David
+const transpose = function(letters) {
 
-module.exports = wordSearch
+  const newMatrix = [];
+  for (let i = 0;  i < letters[0].length; i++) {
+    newMatrix[i] = [];
+    for (let j = 0;  j < letters.length; j++) {
+      newMatrix[i][j] = letters[j][i];
+    }
+  } return newMatrix;
+};
+
+//add vertical line join using transpose
+
+//search for word in horizontal line join
+
+//search for word in vertical line join
+
+//Word search from Ilia and Ben
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let j of verticalJoin) {
+    if (j.includes(word)) return true;
+  } return false;
+};
+
+module.exports = wordSearch;
